37993cdcff4d7df3f0dd12ae68f068eac9b915f6
Astha <astha2333@gmail.com>
2023-03-01 23:40:10 +0530

    Backend

--- indexpost.js
+++ indexpost.js
@@ -14,6 +14,8 @@ app.post("/api/user", (req, res) => {
     console.log(req.body.name);
     console.log(req.body.address.dist);
     console.log(req.body.pincode);
+    console.log(req.body.email);
+    console.log(req.body.password);
 
     //Now we are going to create a new object with above values with different keys.
     //We make a new object which have keys as per the database definition and we fill the corresponding
@@ -23,12 +25,38 @@ app.post("/api/user", (req, res) => {
       userstate: req.body.address.state,
       userdist: req.body.address.dist,
       userpin: req.body.pincode,
+      useremail: req.body.email,
+      userpassword: req.body.password,
     };
     console.log(newobject);
 
     //MongoDB stores documents data in the form of an object and the key names of the objects must be same
     //for each document in a collection which is predefined (in models)
     return res.json({ success: true, message: "Testing submit api" });
+  } catch (error) {
+    return res.status(400).json({ success: false, error: error.message });
+  }
+});
+
+app.post("/api/login", (req, res) => {
+  try {
+    let originalPassword = "needanap";
+    let enteredPassword = req.body.password;
+    if (originalPassword === enteredPassword) {
+      return (
+        res
+          .status(200)
+          //.json({ success: true, message: "Logged in successfully" });
+          .json({
+            success: true,
+            message: `Hello, the email is ${req.body.email} and the password is ${req.body.password}`,
+          })
+      );
+    } else {
+      return res
+        .status(401)
+        .json({ success: false, message: "Please check your password" });
+    }
   } catch (error) {
     return res.status(400).json({ success: false, error: error.message });
   }
