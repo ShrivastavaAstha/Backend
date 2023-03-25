2f1c47c8500befdd7723f964054a25201b081caf
Astha <astha2333@gmail.com>
2023-03-25 23:54:57 +0530

    commit

--- async.js
+++ async.js
@@ -1,3 +1,4 @@
+//Synchronous Function
 const timeTakingFunction = () => {
   return "I will take time";
 };
@@ -11,6 +12,8 @@ callingSyncFunction();
 console.log("After Time taking function");
 
 console.log("--------");
+//Async way
+//1. Use the keyword async before the time taking function declaration
 
 const timetakingFunction = async () => {
   return "I will take time";
